8118e7a71c3251a3cc31db20b9ece48f86262621
TC CRAW <tccrawford007@gmail.com>
2022-08-14 22:08:14 -0500

    finished pb project

--- designs.js
+++ designs.js
@@ -1,10 +1,50 @@
 // Select color input
-// Select size input
+function colorInput(){
+    const colorPicker = document.getElementById("colorPicker")
+    const cells = document.getElementsByClassName("cell")
 
+    for(let i = 0; i , cells.length; i++){
+        cells[i].addEventListener("click", function(e){
+            let clickedCell = e.target;
+            clickedCell.style.backgroundColor = colorPicker.value
+        })
+    }
+}
+// Select size input
+function sizeInput(e){
+    e.preventDefault()
+    const height = document.getElementById("inputHeight").value
+    const width = document.getElementById("inputWidth").value
+    makeGrid(height, width)
+    
+}
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
 
-// Your code goes here!
+function makeGrid(height, width) {
 
+    let table = document.getElementById("pixelCanvas")
+
+    for(let i = 0; i < height; i++){
+        let rowHeight = document.createElement("tr")
+        rowHeight.id = "row" + i
+
+        table.appendChild(rowHeight)
+        let rowWidth = document.getElementById("row" + i)
+
+        for(let j = 0; j < width; j++){
+            let cell = document.createElement("td")
+            cell.id = "cell" + j
+            cell.className = "cell"
+            rowWidth.appendChild(cell)
+        }
+        
+    }
+    console.log(height)
+    colorInput()
+  
 }
+
+let btn = document.getElementById("button")
+btn.addEventListener("click", sizeInput)
+
